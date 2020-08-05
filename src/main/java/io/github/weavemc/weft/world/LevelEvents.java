16911e889da8d42493bec5db9d3698e00ef3eb69
OroArmor <eliorona@live.com>
2020-08-05 13:40:13 -0700

    Some world event unpicks

---
+++ src/main/java/io/github/weavemc/weft/world/LevelEvents.java
@@ -0,0 +1,65 @@
+package io.github.weavemc.weft.world;
+
+public class LevelEvents
+{
+	public static final int DispenserDispense = 1000;
+	public static final int DispenserFail = 1001;
+	public static final int DispenserLaunch = 1002;
+	public static final int EnderEyeLaunch = 1003;
+	public static final int FireworkShoot = 1004;
+	public static final int IronDoorOpen = 1005;
+	public static final int WoodenDoorOpen = 1006;
+	public static final int WoodenTrapdoorOpen = 1007;
+	public static final int FenceGateOpen = 1008;
+	public static final int FireExtinguish = 1009;
+	public static final int PlayMusicDisc = 1010;
+	public static final int IronDoorClose = 1011;
+	public static final int WoodenDoorClose = 1012;
+	public static final int WoodenTrapdoorClose = 1013;
+	public static final int FenceGateClose = 1014;
+	public static final int GhastWarn = 1015;
+	public static final int GhastShoot = 1016;
+	public static final int EnderDragonShoot = 1017;
+	public static final int BlazeShoot = 1018;
+	public static final int ZombieAttackWoodenDoor = 1019;
+	public static final int ZombieAttackIronDoor = 1020;
+	public static final int ZombieBreakWoodenDoor = 1021;
+	public static final int WitherBreakBlock = 1022;
+	public static final int WitherShoot = 1024;
+	public static final int BatTakeoff = 1025;
+	public static final int ZombieInfect = 1026;
+	public static final int ZombieVillagerConverted = 1027;
+	public static final int AnvilDestroy = 1029;
+	public static final int AnvilUse = 1030;
+	public static final int AnvilLand = 1031;
+	public static final int PortalTravel = 1032;
+	public static final int ChorusFlowerGrow = 1033;
+	public static final int ChorusFlowerDeath = 1034;
+	public static final int BrewingStandBrew = 1035;
+	public static final int IronTrapdoorClose = 1036;
+	public static final int IronTrapdoorOpen = 1037;
+	public static final int PhantomBite = 1039;
+	public static final int ZombieToDrowned = 1040;
+	public static final int HuskToZombie = 1041;
+	public static final int GrindstoneUse = 1042;
+	public static final int BookPageTurn = 1043;
+	public static final int SmithingTableUse = 1044;
+	public static final int DragonGrowl = 3001;
+
+	public static final int ComposterFillActions = 1500;
+	public static final int LavaExtinguishAndSmokeParticles = 1501;
+	public static final int RedstoneTorchBurnoutAndSmokeParticles = 1502;
+	public static final int EndPortalFrameFillAndSmokeParticles = 1503;
+	public static final int BlockBreakSoundAndParticles = 2001;
+	public static final int SplashPotionBreak2002 = 2002;
+	public static final int SplashPotionBreak2007 = 2007;
+	public static final int EnderEyeFall = 2003;
+	public static final int EnderEyeBreak = 2004;
+	public static final int DragonFireballBreak = 2006;
+	public static final int EndGatewaySpawn = 3000;
+
+	public static final int SmokeParticles = 2000;
+	public static final int BonemealParticles = 2005;
+	public static final int Explosion = 2008;
+	public static final int Cloud = 2009;
+}
